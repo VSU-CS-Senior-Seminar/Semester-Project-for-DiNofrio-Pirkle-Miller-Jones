39c114c3ff746117b920ec65f3323a62345814b5
jojones90 <josepjones@valdosta.edu>
2016-03-10 19:41:44 +0000

    Added confirmation+unlock tokens using devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
+  #enum role: [:Standard, :Admin, :Business, :Agency]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
 end
