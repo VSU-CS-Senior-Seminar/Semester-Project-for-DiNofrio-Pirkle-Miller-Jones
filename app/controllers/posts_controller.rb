d662bce27e05a594117d17c3f2fcf08e0286c58f
acpirkle <acpirkle@valdosta.edu>
2016-03-22 20:28:43 +0000

    merged comment branch and fixed broken link

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to newsfeed_path, notice: 'Post was successfully created.' }
+        format.html { redirect_to newsfeed_index_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
