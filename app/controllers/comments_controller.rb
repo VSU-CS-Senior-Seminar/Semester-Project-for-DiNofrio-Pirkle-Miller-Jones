e00c1916974773e8e8a57e75a4ddfc821707b39b
jojones90 <josepjones@valdosta.edu>
2016-03-31 19:05:05 +0000

    Added username to comments/posts, users can only see specific zipcode posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -32,7 +32,7 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
     @comment.post_id = params[:post_id]
-    
+    @comment.username = current_user.name
     respond_to do |format|
       if @comment.save
         format.html { redirect_to post_comments_path }
