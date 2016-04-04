ddc688721c3e7c838347d4e7cec1ef68a5592132
cjdinofrio <cjdinofrio@valdosta.edu>
2016-04-04 20:25:17 +0000

    Admins no longer need confirmation emails.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,28 +8,36 @@
 #   check for commit
 User.delete_all
 User.transaction do
-User.create( :name => 'CJ',
+@user = User.new( :name => 'CJ',
              :email => 'cjdinofrio@valdosta.edu',
              :password => 'blahblah', 
              :password_confirmation => 'blahblah',
              :zipcode => '30552',
              :role => 0)
-User.create( :name => 'Joe',
+@user.skip_confirmation!
+@user.save!
+@user = User.new( :name => 'Joe',
              :email => 'josepjones@valdosta.edu',
              :password => 'joseph210', 
              :password_confirmation => 'joseph210',
              :zipcode => '30552',
              :role => 0)
-User.create( :name => 'Tayler',
+@user.skip_confirmation!
+@user.save!
+@user = User.new( :name => 'Tayler',
              :email => 'tcmiller@valdosta.edu',
              :password => 'herpderp', 
              :password_confirmation => 'herpderp',
              :zipcode => '31539',
              :role => 0)
-User.create( :name => 'Andrew',
+@user.skip_confirmation!
+@user.save!
+@user = User.new( :name => 'Andrew',
              :email => 'acpirkle@valdosta.edu',
              :password => 'password', 
              :password_confirmation => 'password',
              :zipcode => '31539',
              :role => 0)
+@user.skip_confirmation!
+@user.save!
 end
